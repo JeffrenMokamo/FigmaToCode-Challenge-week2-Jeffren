6030b0129fdf09c4c05f6a58171b1ecaf556c1f3
jeffren <jeffrenmokamo726@gmail.com>
2024-02-02 01:29:24 +0100

    FIN

--- javascript/script.js
+++ javascript/script.js
@@ -19,4 +19,13 @@ btnViewNav.addEventListener('click', function(){
 
     navBar.classList.toggle('visibilityNavbar');
 
+    if(navBar.classList.contains('visibilityNavbar')){
+        this.classList.remove('bi-list');
+        this.classList.add('bi-x');
+    }
+    else{
+        this.classList.add('bi-list');
+        this.classList.remove('bi-x');
+    }
+
 })
